db32bec1e5b75f89abf3f5b4d70d4d48b261e4cc
Conor Hastings <hastings.conorm@gmail.com>
2016-04-03 16:20:26 -0400

    add test for conditional expression identifier

---
+++ test/__tests__/transform-cond-expression-identifer.js
@@ -0,0 +1,5 @@
+describe('properly transform style assigned in conditional expression when options are identifiers', () => {
+  it('should transform correctly', () => {
+    test('test-cond-expression-identifier');
+  });
+});
